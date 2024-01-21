6ec56bda1e25e20b495e62858b6a7ff9d0343f5d
Nick <nduplessis11@gmail.com>
2024-01-21 18:53:21 -0500

    Update build system configuration

--- compass/src/entry-point.h
+++ compass/src/entry-point.h
@@ -3,7 +3,7 @@
 #include "application.h"
 #include <memory>
 
-int main(int argc, char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
     auto app = std::unique_ptr<Compass::Application>(Compass::CreateApplication());
 	app->Run();
